377575a53940d2fb81106cad0e62a4e99cf97e31
Linh Tuan Nguyen <52875747+silentlie@users.noreply.github.com>
2025-07-23 16:19:52 +1000

    misc: apply google java style format

--- src/main/java/com/example/tetris/HelloController.java
+++ src/main/java/com/example/tetris/HelloController.java
@@ -3,12 +3,20 @@ package com.example.tetris;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * The type Hello controller.
+ */
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+  @FXML
+  private Label welcomeText;
+
+  /**
+   * On the hello button click.
+   */
+  @FXML
+  protected void onHelloButtonClick() {
+
+    welcomeText.setText("Welcome to JavaFX Application!");
+  }
 }
